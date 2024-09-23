b8974ed7080f79f050d803794e185e15d4c12569
Inbar <inbar.lahat@post.idc.ac.il>
2024-09-23 22:42:52 +0300

    another try on search + extra design fixes

--- client/src/components/MentorsDetails.js
+++ client/src/components/MentorsDetails.js
@@ -54,23 +54,8 @@ const MentorsDetails = () => {
 
   return (
     <div className="mentors-container">
-      <h2 className="mentors-title">Take a look at our amazing mentors</h2>
-      
-      <div className="navbar-search-container mb-3">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="search-icon">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-4.35-4.35m1.35-6.65a7.5 7.5 0 1 0-15 0 7.5 7.5 0 0 0 15 0z" />
-        </svg>
-        <input
-          type="text"
-          placeholder="Search"
-          className="search-bar"
-          value={searchWord}
-          onChange={handleSearch}  // Update search as user types
-        />
-      </div>
-
       <div className="card-container">
-        {mentors.length === 0 && <div>No mentors found.</div>}
+        {mentors.length === 0 && <div>No mentors found</div>}
         {mentors.map((mentor) => (
           <MentorCard key={mentor.email} mentor={mentor} />
         ))}
--- client/src/pages/MentorsBrowse.js
+++ client/src/pages/MentorsBrowse.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import MentorsDetails from "../components/MentorsDetails";
-import './Auth.css'; 
+import "./Auth.css";
+import { alignPropType } from "react-bootstrap/esm/types";
 
 const MentorsBrowse = () => {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <section className="home-subtitle">
-      <input
-        type="text"
-        placeholder="Search for mentors..."
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+    <>
+      <div className="navbar-search-container mb-3" >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="search-icon"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-4.35-4.35m1.35-6.65a7.5 7.5 0 1 0-15 0 7.5 7.5 0 0 0 15 0z"
+          />
+        </svg>
+        <input
+          type="text"
+          placeholder="Search"
+          className="search-bar"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          />
+      </div>
       <MentorsDetails searchTerm={searchTerm} />
-    </section>
+    </>
   );
 };
 
