c3461e82bc52e57c7d241a7c0b830be6c0504472
shakedmatityahu <matshake@post.bgu.ac.il>
2024-09-24 16:01:50 +0300

    add photos  import

--- client/src/components/MentorCard.js
+++ client/src/components/MentorCard.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 import avatarImages from "./avatarImages";
+import person1 from "../images/person1.jpg";
+import person2 from "../images/person2.jpg";
+import person3 from "../images/person3.jpg";
+import person4 from "../images/person4.jpg";
+import person5 from "../images/person5.jpg";
+import person6 from "../images/person6.jpg";
+import person7 from "../images/person7.jpg";
+
 import "./MentorCard.css"; // Import custom styles for the grid
 
 const images = [person1, person2, person3, person4, person5, person6, person7];
